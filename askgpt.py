a2e00ca3f3f01d92a174b0ef4390e2c19b2fb397
Jonathan Addington <jonathan.addington@jmaddington.com>
2023-03-16 09:02:31 -0400

    improvements, class based

--- askgpt.py
+++ askgpt.py
@@ -11,6 +11,10 @@ openai.api_key = os.environ["OPENAI_API_KEY"]
 history_file = os.path.expanduser('~/chatgpthistory.sqlite')
 class ChatGPT:
     
+    LastPrompt = ""
+    LastResponse = ""
+    FileContents = ""
+    
     def __init__(self, api_key = os.environ["OPENAI_API_KEY"], history_file = history_file, max_tokens=2048, temperature=0.9, frequency_penalty=0.0, stop=None):
         openai.api_key = api_key
         self.history_file = os.path.expanduser(history_file)
@@ -26,6 +30,9 @@ class ChatGPT:
             self._init_database()
  
     def chat(self, prompt):
+        
+        self.LastPrompt = prompt
+        
         messages = [{"role": "system", "content": "You are a helpful assistant to a user. The user is trying to get a task done. You can help the user by answering their questions. You can also ask the user for more information if you need it."},
                     {"role": "user", "content": "I am going to give you our previous history and then you can respond to my immedieate need."}]
         entries = self.get_last_entries()
@@ -35,8 +42,8 @@ class ChatGPT:
             historical_prompt = entry['prompt']
             message = entry['message']
             
-            messages.append({"role": "user", "content": historical_prompt})
-            messages.append({"role": "system", "content": message})
+            messages.append({"role": "user", "content": f"At {timestamp}: {historical_prompt}"})
+            messages.append({"role": "system", "content": f"At {timestamp}: {message}"})
  
         messages.append({"role": "user", "content": prompt})
         print("...")
@@ -48,8 +55,9 @@ class ChatGPT:
         )
 
         message = response.choices[0].message.content
+        self.LastResponse = message
  
-        return message
+        return self.LastResponse
  
     def save_chat(self, prompt, message):
         conn = sqlite3.connect(self.history_file)
@@ -66,7 +74,7 @@ class ChatGPT:
         conn.commit()
         conn.close()
  
-    def get_last_entries(self, n=4):
+    def get_last_entries(self, n=10):
         conn = sqlite3.connect(self.history_file)
         conn.row_factory = sqlite3.Row
         c = conn.cursor()
@@ -84,17 +92,74 @@ class ChatGPT:
         with open("better.py", "w") as file:
             file.write(response)
         print(f"Better script written to better.py")
-
-
+        
+    def StartChat(self):
+        while True:
+            prompt = self.AskForInput()
+            response = self.chat(prompt)
+            print(f"{response}\n...\n")
+            
+    def RespondToCode(self, hint = 'What do you want to do with the file??'):
+        if not self.FileContents:
+            print("No file contents to respond to.")
+            exit(1)
+            
+        prompt = self.AskForInput(hint)
+        
+        prompt = f"{prompt}\n\n{self.FileContents}"
+        
+        response = self.chat(prompt)
+        print (f"{response}")
+                
+    def AskForInput(self, hint = 'Ask me anything! (Enter two consecutive empty lines to exit)'):
+        """
+        Ask the user for input until two blank lines are entered.
+        
+        :return: The user input as a single string
+        """
+        while True:
+            # Initialize an empty list to hold the user inputs
+            print(hint)
+            user_inputs = []
+            
+            # Keep accepting input until two consecutive empty lines are entered
+            while len(user_inputs) < 2 or user_inputs[-2:] != ["", ""]:
+                user_input = input()
+                
+                if user_input == "```":
+                    while user_input != "```":
+                        user_input = input()
+                        user_inputs.append(user_input)
+                
+                    if user_input == "exit" or user_input == "quit":
+                        exit(0)
+                        
+                elif user_input == "exit" or user_input == "quit":
+                    exit(0)
+                    
+                user_inputs.append(user_input)
+                
+            # Remove the two consecutive empty lines from the user inputs
+            user_inputs = user_inputs[:-2]
+            
+            # Combine the individual lines into a single string
+            prompt = "\n".join(user_inputs)
+            
+            if not prompt.strip():
+                print("You didn't enter a prompt. Please try again.")
+            else:
+                return prompt
+            
 if __name__ == '__main__':
 
     # Set up parser
     parser = argparse.ArgumentParser(description='Interact with the OpenAI GPT-3 API')
-    parser.add_argument('-p', '--prompt', type=str, help='The prompt to provide to the API')
+    # parser.add_argument('-p', '--prompt', type=str, help='The prompt to provide to the API')
     parser.add_argument('-s', '--stop', type=str, default=None, help='The stop sequence for the API to use')
+    parser.add_argument('--code', type=str, help='Read the file as code, then ask what to do with it.')
 
     # Add extra arguments
-    parser.add_argument('--max_tokens', type=int, help='The maximum number of tokens to generate in the chat response', default=2048)
+    parser.add_argument('--max_tokens', type=int, help='The maximum number of tokens to generate in the chat response', default=3000)
 
     parser.add_argument('--temperature', type=float, default=.7,
                                 help='The softmax temperature to use for the API response')
@@ -104,32 +169,26 @@ if __name__ == '__main__':
     parser.add_argument('--better', '-b', action='store_true', help='Use GPT to improve the script')
     args = parser.parse_args()
 
-    chat = ChatGPT(max_tokens=args.max_tokens, temperature=args.temperature, frequency_penalty=args.frequency_penalty)
+    bot = chat = ChatGPT(max_tokens=args.max_tokens, temperature=args.temperature, frequency_penalty=args.frequency_penalty)
 
     if args.better:
         chat.metaBetter()
         sys.exit()
+        
     elif args.last:
         entries = chat.get_last_entries(history_file, args.last)
         for entry in entries:
             print(f"{entry[1]} - You: {entry[2]}\nChatGPT: {entry[3]}\n")
-    elif args.prompt:
-        prompt = args.prompt
-        response = chat.chat(prompt)
-        while response is None:
-            time.sleep(5)
-            response = chat.chat(prompt)
-        print(f"{response}\n..")
-        chat.save_chat(history_file, prompt, response)
+        
+    elif args.code:
+        file = open(args.code, "r")
+        bot.FileContents = file.read()
+        file.close()
+        bot.RespondToCode()
+        
+    # elif args.prompt:
+    #     bot.StartChat()
+        
     else:
-        while True:
-            prompt = sys.stdin.read()
-            if prompt.strip().lower() == 'q':
-                sys.exit()
-            response = chat.chat(prompt)
-            while response is None:
-                print("ChatGPT: I'm sorry, I encountered an error. Could you please rephrase your input?")
-                error_prompt = sys.stdin.read()
-                response = chat(prompt + error_prompt)
-            print("ChatGPT: " + response)
-            chat.save_chat(prompt, response)
+        print(f"\nYou can use ``` to enter code. End code block with ``` and still use two consecutive empty lines to exit.")
+        bot.StartChat()
